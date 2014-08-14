c3290e7ebd461458cb33a973f7c46a5f2b49081e
Antoine van Gelder <antoine@trigger.io>
2014-08-14 16:01:09 +0200

    Unify tests

--- module/tests/interactive.js
+++ module/tests/interactive.js
@@ -1,41 +1,22 @@
 module("platform");
 
-if (forge.is.android()) {
-  asyncTest("Version data", 1, function() {
-    forge.platform.getVersion(function(version) {
-      forge.platform.getModel(function(model) {
-        forge.platform.getAPILevel(function(level) {
-          forge.platform.getManufacturer(function(man) {
-            askQuestion("Is this device version " + version + ", API level "  + level + " and a " + man + " " + model + "?", {
-              Yes : function() {
-                ok(true, "Success");
-                start();
-              },
-              No : function() {
-                ok(false, "User claims failure");
-                start();
-              }
-            });
+asyncTest("Version data", 1, function() {
+  forge.platform.getVersion(function(version) {
+    forge.platform.getModel(function(model) {
+      forge.platform.getAPILevel(function(level) {
+        forge.platform.getManufacturer(function(man) {
+          askQuestion("Is this device version " + version + ", API level "  + level + " and a " + man + " " + model + "?", {
+            Yes : function() {
+              ok(true, "Success");
+              start();
+            },
+            No : function() {
+              ok(false, "User claims failure");
+              start();
+            }
           });
         });
       });
     });
   });
-} else {
-  asyncTest("Version data", 1, function() {
-    forge.platform.getVersion(function(version) {
-      forge.platform.getModel(function(model) {
-        askQuestion("Is this device version " + version + " and a " + model + "?", {
-          Yes : function() {
-            ok(true, "Success");
-            start();
-          },
-          No : function() {
-            ok(false, "User claims failure");
-            start();
-          }
-        });
-      });
-    });
-  });
-}
+});
