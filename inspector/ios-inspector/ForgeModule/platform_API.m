5c6759f244452325113861fa0bd3910baa4c53fd
Patrick Rudolph <patrick@hojoki.com>
2014-09-17 10:19:40 +0200

    updated iOS device identifiers

--- inspector/ios-inspector/ForgeModule/platform_API.m
+++ inspector/ios-inspector/ForgeModule/platform_API.m
@@ -14,6 +14,8 @@
 }
 
 // return the device model name
+// http://theiphonewiki.com/wiki/Models
+// http://en.wikipedia.org/wiki/List_of_iOS_devices
 + (void)getModel:(ForgeTask*)task {
     struct utsname systemInfo;
     uname(&systemInfo);
@@ -25,6 +27,7 @@
     if ([platform isEqualToString:@"iPhone1,2"])    [task success:@"iPhone 3G"];
     if ([platform isEqualToString:@"iPhone2,1"])    [task success:@"iPhone 3GS"];
     if ([platform isEqualToString:@"iPhone3,1"])    [task success:@"iPhone 4"];
+    if ([platform isEqualToString:@"iPhone3,2"])    [task success:@"iPhone 4"];
     if ([platform isEqualToString:@"iPhone3,3"])    [task success:@"Verizon iPhone 4"];
     if ([platform isEqualToString:@"iPhone4,1"])    [task success:@"iPhone 4S"];
     if ([platform isEqualToString:@"iPhone5,1"])    [task success:@"iPhone 5 (GSM)"];
@@ -33,6 +36,8 @@
     if ([platform isEqualToString:@"iPhone5,4"])    [task success:@"iPhone 5c (GSM+CDMA)"];
     if ([platform isEqualToString:@"iPhone6,1"])    [task success:@"iPhone 5s (GSM)"];
     if ([platform isEqualToString:@"iPhone6,2"])    [task success:@"iPhone 5s (GSM+CDMA)"];
+    if ([platform isEqualToString:@"iPhone7,1"])    [task success:@"iPhone 6 Plus"];
+    if ([platform isEqualToString:@"iPhone7,2"])    [task success:@"iPhone 6"];
     if ([platform isEqualToString:@"iPod1,1"])      [task success:@"iPod Touch 1G"];
     if ([platform isEqualToString:@"iPod2,1"])      [task success:@"iPod Touch 2G"];
     if ([platform isEqualToString:@"iPod3,1"])      [task success:@"iPod Touch 3G"];
@@ -54,8 +59,10 @@
     if ([platform isEqualToString:@"iPad3,6"])      [task success:@"iPad 4 (GSM+CDMA)"];
     if ([platform isEqualToString:@"iPad4,1"])      [task success:@"iPad Air (WiFi)"];
     if ([platform isEqualToString:@"iPad4,2"])      [task success:@"iPad Air (Cellular)"];
+    if ([platform isEqualToString:@"iPad4,3"])      [task success:@"iPad Air (Cellular)"];
     if ([platform isEqualToString:@"iPad4,4"])      [task success:@"iPad mini 2G (WiFi)"];
     if ([platform isEqualToString:@"iPad4,5"])      [task success:@"iPad mini 2G (Cellular)"];
+    if ([platform isEqualToString:@"iPad4,6"])      [task success:@"iPad mini 2G (Cellular)"];
     if ([platform isEqualToString:@"i386"])         [task success:@"Simulator"];
     if ([platform isEqualToString:@"x86_64"])       [task success:@"Simulator"];
     [task success:platform];
