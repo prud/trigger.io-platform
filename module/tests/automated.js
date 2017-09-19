2cf1320bc670eba8d06667f6ade3f5941986e1cb
Antoine van Gelder <antoine@trigger.io>
2017-09-19 13:24:51 +0200

    Bump binaries

--- module/tests/automated.js
+++ module/tests/automated.js
@@ -1,43 +1,43 @@
 module("platform");
 
 asyncTest("Get OS version", 1, function() {
-        forge.platform.getVersion(function () {
-                ok(true, "Got OS version");
-                start();
-        }, function () {
-                ok(false, "API Error");
-                start();
-        });
+    forge.platform.getVersion(function () {
+        ok(true, "Got OS version");
+        start();
+    }, function () {
+        ok(false, "API Error");
+        start();
+    });
 });
 
 asyncTest("Get device model", 1, function() {
-        forge.platform.getModel(function () {
-                ok(true, "Got device model");
-                start();
-        }, function () {
-                ok(false, "API Error");
-                start();
-        });
+    forge.platform.getModel(function () {
+        ok(true, "Got device model");
+        start();
+    }, function () {
+        ok(false, "API Error");
+        start();
+    });
 });
 
 if (forge.is.android()) {
-        asyncTest("Get Android API level", 1, function() {
-                forge.platform.getAPILevel(function () {
-                        ok(true, "Got Android API level");
-                        start();
-                }, function () {
-                        ok(false, "API Error");
-                        start();
-                });
+    asyncTest("Get Android API level", 1, function() {
+        forge.platform.getAPILevel(function () {
+            ok(true, "Got Android API level");
+            start();
+        }, function () {
+            ok(false, "API Error");
+            start();
         });
+    });
 
-        asyncTest("Get Android Manufacturer", 1, function() {
-                forge.platform.getManufacturer(function () {
-                        ok(true, "Got Android Manufacturer");
-                        start();
-                }, function () {
-                        ok(false, "API Error");
-                        start();
-                });
+    asyncTest("Get Android Manufacturer", 1, function() {
+        forge.platform.getManufacturer(function () {
+            ok(true, "Got Android Manufacturer");
+            start();
+        }, function () {
+            ok(false, "API Error");
+            start();
         });
+    });
 }
--- module/tests/interactive.js
+++ module/tests/interactive.js
@@ -1,22 +1,22 @@
 module("platform");
 
 asyncTest("Version data", 1, function() {
-  forge.platform.getVersion(function(version) {
-    forge.platform.getModel(function(model) {
-      forge.platform.getAPILevel(function(level) {
-        forge.platform.getManufacturer(function(man) {
-          askQuestion("Is this device version " + version + ", API level "  + level + " and a " + man + " " + model + "?", {
-            Yes : function() {
-              ok(true, "Success");
-              start();
-            },
-            No : function() {
-              ok(false, "User claims failure");
-              start();
-            }
-          });
+    forge.platform.getVersion(function(version) {
+        forge.platform.getModel(function(model) {
+            forge.platform.getAPILevel(function(level) {
+                forge.platform.getManufacturer(function(man) {
+                    askQuestion("Is this device version " + version + ", API level "  + level + " and a " + man + " " + model + "?", {
+                        Yes : function() {
+                            ok(true, "Success");
+                            start();
+                        },
+                        No : function() {
+                            ok(false, "User claims failure");
+                            start();
+                        }
+                    });
+                });
+            });
         });
-      });
     });
-  });
 });
